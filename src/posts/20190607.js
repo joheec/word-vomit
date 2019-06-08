e0e9bd92df0055fac64b8254ba12677753b7a40f
Johee Chung <johee.chung@nike.com>
2019-06-07 20:04:34 -0700

    Format entry 2

--- src/posts/20190607.js
+++ src/posts/20190607.js
@@ -39,9 +39,11 @@ function Post() {
         <p>All of a sudden, your "constant" variable has a different value and your whole world feels like it's full of lies! Well not exactly. When you have an object or an array, there can be a lot of content, so that variable holds a pointer/reference to where that object/array is stored. That pointer/reference hasn't changed, just the value you were storing there. This is an important distinction to make when working with functions and their parameters.</p>
         <p>Functions use parameters to pass in useful data.</p>
         <Code lines={code.snippetB} title="Example" />
-        <p>After nameOfFunction runs, what are the values of passedInA and passedInB? passedInA will equal 0 because it is not an object or array, and it is passed into nameOfFunction by value. Passed by value means that it makes a copy of the value before handing it off to the function. passInB is not as lucky. It is an array and is passed into nameOfFunction by reference. This means that passedInB is NOT copied and handed off to parameterB to do whatever it wants with it. Instead, it passes the reference to where passedInB is stored, and so if there are any direct changes to parameterB, it will make those changes where passedInB is stored.</p>
+        <p>After nameOfFunction runs, what are the values of passedInA and passedInB?</p>
+        <p>passedInA will equal 0 because it is not an object or array, and it is passed into nameOfFunction by value. Passed by value means that it makes a copy of the value before handing it off to the function.</p>
+        <p>passInB is not as lucky. It is an array and is passed into nameOfFunction by reference. This means that passedInB is NOT copied and handed off to parameterB to do whatever it wants with it. Instead, it passes the reference to where passedInB is stored, and so if there are any direct changes to parameterB, it will make those changes where passedInB is stored.</p>
         <p>So what would passedInB[0] print out? If you said 7, you'd be correct!</p>
-        <p>Not everything is as it seems in JavaScript</p>
+        <p>Not everything is as it seems in JavaScript...</p>
       </Entry>
     </div>
   );
