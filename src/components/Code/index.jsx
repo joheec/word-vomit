8505d467bea89017954adf3bc418ddc19eaf3d69
Johee Chung <johee.chung@nike.com>
2019-06-08 13:48:09 -0700

    Refactor Code component to make newline and tab formatting reusable

--- src/components/Code/index.jsx
+++ src/components/Code/index.jsx
@@ -2,7 +2,32 @@ import React, { Fragment } from 'react';
 import Indent from '../Indent';
 import './styles.css';
 
-function Code({ lines, title='' }) {
+const formatNewline = (id, line) => (
+  line.split('\\n')
+    .reduce((renderLines, line, index) => {
+      return [
+        ...renderLines,
+        {
+          id: id + index,
+          content: line,
+        },
+      ];
+    }, [])
+);
+
+const formatTab = line => (
+  line.content.split('\\t').map((content, index) => (
+    <Fragment key={line.id + index}>
+      <Indent />
+      { content }
+    </Fragment>
+  ))
+);
+
+function Code({ id, title='', children }) {
+  const lines = formatNewline(id, (typeof children === 'object'
+    ? [ ...children ].join('')
+    : children));
   return(
     <div className="code-container">
       <div className="code-title">{title}</div>
@@ -17,8 +42,7 @@ function Code({ lines, title='' }) {
                     <Fragment>&nbsp;&nbsp;</Fragment>
                   </td>
                   <td className="code-lineContent">
-                    <Indent />
-                    { line.content }
+                    { formatTab(line) }
                   </td>
                 </tr>
               );
